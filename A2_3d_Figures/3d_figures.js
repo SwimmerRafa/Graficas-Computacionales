3f4069e29bb612a93244dccbce043edbf92d7265
SwimmerRafa <rafaelmorenoc97@gmail.com>
2020-02-27 15:30:14 -0600

    27/02/2020 15:30:14

--- A2_3d_Figures/3d_figures.js
+++ A2_3d_Figures/3d_figures.js
@@ -72,14 +72,322 @@ function initGL(canvas)
 }
 
 // TO DO: Create the functions for each of the figures.
+
 function createPyramid(gl, translation, rotationAxis){
     let vertexBuffer;
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
     let verts = [
-        //
-    ]
+         //Face 1
+        -.5, 0.0, 1.0,
+        0.0, 2.0, 0.0,
+        .5, 0.0, 1.0,
+
+        //Face2
+        0.5, 0.0, 1.0,
+        0.0, 2.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        //Face3
+        1.0, 0.0, 0.0,
+        0.0, 2.0, 0.0,
+        0.0, 0.0, -1.0,
+
+        //Face4
+        0-0, 0.0, -1.0,
+        0.0, 2.0, 0.0,
+        -1.0, 0.0, 0.0,
+
+        //Face5
+        -1.0, 0.0, 0.0,
+        0.0, 2.0, 0.0,
+        -0.5, 0.0, 1.0,
+
+        //Base
+        -1.0, 0.0, 0.0,
+        -0.5, 0.0, 1.0,
+        0.0, 0.0, -1.0,
+
+        -0.5, 0.0, 1.0,
+        0.5, 0.0, 1.0,
+        0.0, 0.0, -1.0,
+
+        0.5, 0.0, 1.0,
+        1.0, 0.0,0.0,
+        0.0, 0.0, -1.0,
+
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    let faceColors = [
+        [1.0, 0.0, 0.0, 1.0], // Front face
+        [0.0, 1.0, 0.0, 1.0], // Back face
+        [0.0, 0.0, 1.0, 1.0], // Top face
+        [1.0, 1.0, 0.0, 1.0], // Bottom face
+        [1.0, 0.0, 1.0, 1.0], // Right face
+        [0.0, 1.0, 1.0, 1.0], // Left face
+        [0.0, 1.0, 1.0, 1.0],  // Left face
+        [0.0, 1.0, 1.0, 1.0]  // Left face
+    ];
+
+    let vertexColors = [];
+
+    faceColors.forEach(color =>{
+        if(faceColors.indexOf(color) === 5){
+            for (let j=0; j < 5; j++)
+                vertexColors.push(...color);
+        }
+        else{
+            for (let j=0; j < 3; j++)
+                vertexColors.push(...color);
+        }
+    });
+
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+    let pyramidIndxexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndxexBuffer);
+
+    let pyrmidIndices = [
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+        12, 13, 14,
+
+        15, 16, 17,   18, 19, 20,   21, 22, 23//Base
+    ];
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyrmidIndices), gl.STATIC_DRAW);
+
+    let  pyramid = {
+        buffer:vertexBuffer, colorBuffer: colorBuffer,indices:pyramidIndxexBuffer,
+        vertSize:3, nVerts:24, colorSize:4, nColors:6, nIndices:24,
+        primtype:gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime : Date.now()};
+
+    mat4.translate(pyramid.modelViewMatrix, pyramid.modelViewMatrix, translation);
+
+    pyramid.update = function(){
+        let now = Date.now();
+        let deltat = now - this.currentTime;
+        this.currentTime = now;
+        let fract = deltat / duration;
+        let angle = Math.PI * 2 * fract;
+
+        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+    };
+
+    return pyramid;
+}
+
+function createDode(gl, translation, rotationAxis) {
+    let vertexBuffer;
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    let verts = [
+        0, 0.62, 1.62, -1, 1, 1, -0.62, 1.62, 0, 0.62, 1.62, 0, 1, 1, 1,
+
+        0, 0.62, 1.62, 1, 1, 1, 1.62, 0, 0.62, 1, -1, 1, 0, -0.62, 1.62,
+
+        0, -0.62, 1.62, 1, -1, 1, 0.62, -1.62, 0, -0.62, -1.62, 0, -1, -1, 1,
+
+        0, 0.62, 1.62, -1, 1, 1, -1.62, 0, 0.62, -1, -1, 1, 0, -0.62, 1.62,
+
+        0, 0.62, -1.62, 1, 1, -1, 0.62, 1.62, 0, -0.62, 1.62, 0, -1, 1, -1,
+
+        0, 0.62, -1.62, -1, 1, -1, -1.62, 0, -0.62, -1, -1, -1, 0, -0.64, -1.62,
+
+        0, -0.64, -1.62, -1, -1, -1, -0.62, -1.62, 0, 0.62, -1.62, 0, 1, -1, -1,
+
+        0, 0.62, -1.62, 0, -0.62, -1.62, 1, -1, -1, 1.62, 0, -0.62, 1, 1, -1,
+
+        0.62, 1.62, 0, 1, 1, -1, 1.62, 0, -0.62, 1.62, 0, 0.62, 1, 1, 1,
+
+        0.62, -1.62, 0, 1, -1, 1, 1.62, 0, 0.62, 1.62, 0, -0.62, 1, -1, -1,
+
+        -0.62, 1.62, 0, -1, 1, 1, -1.62, 0, 0.62, -1.62, 0, -0.62, -1, 1, -1,
+
+        -0.62, -1.62, 0, -1, -1, -1, -1.62, 0, -0.62, -1.62, 0, 0.62, -1, -1, 1
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    let faceColors = [
+        [1.0, 0.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0, 1.0],
+        [0.2, 0.5, 0.7, 1.0],
+        [0.8, 0.2, 0.4, 1.0],
+        [1.0, 0.5, .7, 1.0],
+        [0.5, 1.0, .2, 1.0],
+        [0.2, 0.6, 0.6, 1.0],
+        [0.7, 1.0, 0.4, 1.0]
+    ];
+
+    let vertexColors = [];
+
+    faceColors.forEach(color => {
+        for (let j = 0; j < 5; j++)
+            vertexColors.push(...color);
+    });
+
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+    let dedoIndxexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, dedoIndxexBuffer);
+
+    let dedoIndices = [
+            0, 1, 2, 0, 2, 3, 0, 3, 4,
+            5, 6, 7, 5, 7, 8, 5, 8, 9,
+            10, 11, 12, 10, 12, 13, 10, 13, 14,
+            15, 16, 17, 15, 17, 18, 15, 18, 19,
+            20, 21, 22, 20, 22, 23, 20, 23, 24,
+            25, 26, 27, 25, 27, 28, 25, 28, 29,
+            30, 31, 32, 30, 32, 33, 30, 33, 34,
+            35, 36, 37, 35, 37, 38, 35, 38, 39,
+            40, 41, 42, 40, 42, 43, 40, 43, 44,
+            45, 46, 47, 45, 47, 48, 45, 48, 49,
+            50, 51, 52, 50, 52, 53, 50, 53, 54,
+            55, 56, 57, 55, 57, 58, 55, 58, 59
+        ];
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(dedoIndices), gl.STATIC_DRAW);
+
+    let dedo = {
+        buffer: vertexBuffer, colorBuffer: colorBuffer, indices: dedoIndxexBuffer,
+        vertSize: 3, nVerts: 60, colorSize: 4, nColors: 12, nIndices: 108,
+        primtype: gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime: Date.now()
+    };
+
+    mat4.translate(dedo.modelViewMatrix, dedo.modelViewMatrix, translation);
+
+    dedo.update = function () {
+        let now = Date.now();
+        let deltat = now - this.currentTime;
+        this.currentTime = now;
+        let fract = deltat / duration;
+        let angle = Math.PI * 2 * fract;
+
+        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+        //mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis2)
+    };
+
+    return dedo;
+}
+
+function createOcta(gl, translation, rotationAxis) {
+    let vertexBuffer;
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    let verts = [
+
+        0, 0, 1,
+        0, 1, 0, //Triangle 1 Top
+        1, 0, 0,
+
+        1, 0, 0,
+        0, 1, 0, //Triangle 2 Top
+        0, 0, -1,
+
+        0, 0, -1,
+        0, 1, 0,  //Triangle 3Top
+        -1, 0, 0,
+
+        -1, 0, 0,
+        0, 1, 0, //Triangle 4 Top
+        0, 0, 1,
+
+        0, 0, 1,
+        0, -1, 0, //Triangle 1 Bottom
+        1, 0, 0,
+
+        1, 0, 0,
+        0, -1, 0, //Triangle 2 Bottom
+        0, 0, -1,
+
+        0, 0, -1,
+        0, -1, 0,  //Triangle 3 Bottom
+        -1, 0, 0,
+
+        -1, 0, 0,
+        0, -1, 0, //Triangle 4 Bottom
+        0, 0, 1,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+    let colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    let faceColors = [
+        [1.0, 0.0, 0.0, 1.0],
+        [0.0, 1.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0, 1.0],
+        [0.0, 1.0, 1.0, 1.0],
+        [0.2, 0.5, 0.7, 1.0],
+        [0.8, 0.2, 0.4, 1.0]
+    ];
+
+    let vertexColors = [];
+
+    faceColors.forEach(color => {
+        for (let j = 0; j < 3; j++)
+            vertexColors.push(...color);
+    });
+
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+    let octaIndxexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, octaIndxexBuffer);
+
+    let octaIndices = [
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+
+        12, 13, 14,
+        15, 16, 17,
+        18, 19, 20,
+        21, 22, 23
+    ];
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(octaIndices), gl.STATIC_DRAW);
+
+    let octa = {
+        buffer: vertexBuffer, colorBuffer: colorBuffer, indices: octaIndxexBuffer,
+        vertSize: 3, nVerts: 24, colorSize: 4, nColors: 8, nIndices: 24,
+        primtype: gl.TRIANGLES, modelViewMatrix: mat4.create(), currentTime: Date.now()
+    };
+
+    mat4.translate(octa.modelViewMatrix, octa.modelViewMatrix, translation);
+
+    octa.update = function () {
+        let now = Date.now();
+        let deltat = now - this.currentTime;
+        this.currentTime = now;
+        let fract = deltat / duration;
+        let angle = Math.PI * 2 * fract;
+
+        mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+    };
+
+    return octa;
 }
 
 function createShader(gl, str, type)
